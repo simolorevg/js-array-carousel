32c912e17e281cd125daee433367faeee6687286
simolorevg <simoneviaggi@libero.it>
2023-03-14 16:06:13 +0100

    start the script again

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 //creo array di immagini
 const imgArray = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
+const imgSection = document.querySelector('.img-section'); //creo variabile per individuare la sezione delle immagini
+console.log(imgSection);
+for(let i = 0; i < imgArray.length; i++){
+    let currentImg = imgArray[i];// creo variabile per effettuare assegnaione di immagine all'interno di img-section
+}
