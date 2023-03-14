a1a150a8f8bb3f68384dffa90b139afd472b192b
simolorevg <simoneviaggi@libero.it>
2023-03-14 16:28:33 +0100

    Working on script

--- js/script.js
+++ js/script.js
@@ -4,4 +4,22 @@ const imgSection = document.querySelector('.img-section'); //creo variabile per 
 console.log(imgSection);
 for(let i = 0; i < imgArray.length; i++){
     let currentImg = imgArray[i];// creo variabile per effettuare assegnaione di immagine all'interno di img-section
-}
+    imgSection.innerHTML += `<div class="img-container">
+    <img src="${currentImg}" alt="">
+    </div>`;
+    console.log(imgSection);
+};
+//creo la sequenza
+imgContainerArray=document.getElementsByClassName('img-container');//genero l'array di div contenenti le immagini
+console.log(imgContainerArray);
+let containerIndex = 0;//imposto l'indice a zero
+imgContainerArray[0].classList.add("active");//parte dalla prima immagine
+const upBtn = document.querySelector('.up');
+upBtn.addEventListener("click", function() {
+    imgContainerArray[containerIndex].classList.remove("active");
+    containerIndex++;
+    imgContainerArray[containerIndex].classList.add('active');
+    if(containerIndex === imgContainerArray.length - 1){ //se arrivo in fondo al carosello mi nasconde il bottone
+        upBtn.classList.add("hidden");
+    }
+})
