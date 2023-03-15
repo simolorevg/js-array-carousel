d6caa0a6df111f8b5b92a5192a926ca3689f06f1
simolorevg <simoneviaggi@libero.it>
2023-03-15 10:31:19 +0100

    Fixed the problem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const imgArray = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];//creo array di immagini
+const imgArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];//creo array di immagini
 const imgSection = document.querySelector('.img-section'); //creo variabile per individuare la sezione delle immagini
 console.log(imgSection);
-for(let i = 0; i < imgArray.length; i++){
+for (let i = 0; i < imgArray.length; i++) {
     let currentImg = imgArray[i];// creo variabile per effettuare assegnaione di immagine all'interno di img-section
     imgSection.innerHTML += `<div class="img-container">
     <img src="${currentImg}" alt="">
@@ -9,36 +9,35 @@ for(let i = 0; i < imgArray.length; i++){
     console.log(imgSection);
 };
 //creo la sequenza
-imgContainerArray=document.getElementsByClassName('img-container');//genero l'array di div contenenti le immagini
+imgContainerArray = document.getElementsByClassName('img-container');//genero l'array di div contenenti le immagini
 console.log(imgContainerArray);
-let containerArrayLenght = imgContainerArray.length;//questo mi servirà per il superbonus
 let containerIndex = 0;//imposto l'indice a zero
 imgContainerArray[0].classList.add("active");//parte dalla prima immagine
 const upBtn = document.querySelector('.up');
 const downBtn = document.querySelector('.down');//creo le variabili per poter utilizzare i pulsanti up e down
-downBtn.classList.add('hidden');//nascondo inizialmente il bottone down
-upBtn.addEventListener("click", function() {
+upBtn.addEventListener("click", function () {
     downBtn.classList.remove('hidden');//faccio riapparire il pulsante down
-    if(containerIndex === imgContainerArray.length - 1){ //se arrivo in fondo al carosello mi fa ripartire da capo
+    if (containerIndex === imgContainerArray.length - 1) { //se arrivo in fondo al carosello mi fa ripartire da capo
         // upBtn.classList.add("hidden");
         imgContainerArray[containerIndex].classList.remove('active');//rimuovo la classe active all'immagine corrente
         containerIndex = 0;//riporto all'inizio l'indice
         imgContainerArray[containerIndex].classList.add('active');//aggiungo active all'immagine successiva
-    }else{
+    } else {
         imgContainerArray[containerIndex].classList.remove("active");//nascondo l'immagine corrente
         containerIndex++;//incremento di uno l'indice
         imgContainerArray[containerIndex].classList.add('active');//faccio apparire l'immagine successiva
     }
     console.log(containerIndex);
 });
-downBtn.addEventListener("click", function(){
+let containerArrayLenght = imgContainerArray.length;//questo mi servirà per il superbonus
+downBtn.addEventListener("click", function () {
     upBtn.classList.remove('hidden');// faccio riapparire i pulsante up
-    if(containerIndex === 0){ //se arrivo in fondo al carosello ricomincio da capo
+    if (containerIndex === 0) { //se arrivo in fondo al carosello ricomincio da capo
         // downBtn.classList.add("hidden");
         imgContainerArray[containerIndex].classList.remove('active');
-        containerIndex = containerArrayLenght;
+        containerIndex = containerArrayLenght - 1;
         imgContainerArray[containerIndex].classList.add('active');
-    }else{
+    } else {
         imgContainerArray[containerIndex].classList.remove("active");//nascondo l'immagine corrente
         containerIndex--;//decremento di uno l'indice
         imgContainerArray[containerIndex].classList.add('active');//faccio apparire l'immagine successiva
